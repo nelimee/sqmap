9c74ac5105997b2dac583c298ece823660979ad0
Nelimee <adrien.suau@cerfacs.fr>
2022-03-13 10:32:11 +0100

    Removing non-used axes for whole-chip visualisation

--- src/sqmap/visualisation/chip.py
+++ src/sqmap/visualisation/chip.py
@@ -26,9 +26,15 @@ def plot_whole_chip_view(
         )
 
     fig, axes = plt.subplots(placement.max_y + 1, placement.max_x + 1)
+    fig.set_tight_layout(True)
     for qubit_index, (x, y) in enumerate(placement.positions):
         ideal_points = [dm[0] for dm in density_matrices[qubit_index]]
         density_matrices_on_points = [dm[1] for dm in density_matrices[qubit_index]]
         plot_bloch_vector_displacement_arrow_field_2d(
             ideal_points, density_matrices_on_points, fig=fig, ax=axes[y, x]
         )
+    all_axes: ty.Set[ty.Tuple[int, int]] = {
+        (i, j) for i in range(placement.max_x + 1) for j in range(placement.max_y + 1)
+    }
+    for x, y in all_axes.difference(placement.positions):
+        axes[y, x].remove()
