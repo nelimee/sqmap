e255849977c8c188a7489e3f65f5526763675c6a
Adrien Suau <adrien.suau+github@protonmail.com>
2024-04-04 11:28:19 +0200

    Code refresh (#3)
    * 📦 setup.py -> pyproject.toml
    
    * 🎨 Update code to new qiskit version
    
    * 📝 Update CHANGELOG

---
+++ CHANGELOG.md
@@ -0,0 +1,18 @@
+# Changelog
+
+## [0.1.0]
+
+### Added
+
+- `pyproject.toml` packaging.
+
+### Removed
+
+- `setup.py` and `setup.cfg` packaging.
+- Chip view and backend-related qubit placement.
+
+### Improved
+
+- Support for Qiskit 1.0.
+
+### Deprecated
--- examples/fake_backend_delay.py
+++ examples/fake_backend_delay.py
@@ -3,46 +3,59 @@ import typing as ty
 import numpy
 import matplotlib.pyplot as plt
 
-from qiskit import IBMQ
-from qiskit.providers.aer import AerSimulator
+from qiskit_aer import AerSimulator
+from qiskit_ibm_runtime import QiskitRuntimeService
 
 from sqt.circuits import one_qubit_tomography_circuits
 from sqt.basis.tetrahedral import TetrahedralMeasurementBasis
 from sqt.fit.mle import post_process_tomography_results_mle
 
-from sqmap.circuits import get_approximately_equidistant_circuits
+from sqt.basis.equidistant import get_approximately_equidistant_circuits
 from sqmap.visualisation.flatmap import plot_bloch_vector_displacement_arrow_field_2d
 
-n: int = 100
+# %%
+hub = "ibm-q-lanl"
+group = "lanl"
+project = "quantum-simulati"
+backend_name = "ibm_algiers"
 
-circuits = get_approximately_equidistant_circuits(n)
+print(f"Recovering IBMQ backend {backend_name} with {hub}/{group}/{project}.")
+service = QiskitRuntimeService(
+    channel="ibm_quantum", instance=f"{hub}/{group}/{project}"
+)
+if not service.active_account():
+    raise RuntimeError(f"Could not load account with '{hub}' '{group}' '{project}'.")
+backend = service.get_backend("ibm_algiers")
 
-for c in circuits:
-    c.delay(32 * 20000, 0)
-
-print("Recovering data from IBMQ...")
-if not IBMQ.active_account():
-    IBMQ.load_account()
-provider = IBMQ.get_provider(hub="ibm-q-lanl", group="lanl", project="quantum-simulati")
-backend = provider.get_backend("ibmq_bogota")
+# %%
+print(f"Initialising a noisy simulator with {backend_name} calibrations.")
 simulator = AerSimulator.from_backend(backend)
+qubit_number: int = 1
+max_shots: int = 2**15
+nb_points_on_bloch_sphere: int = 100
+
+circuits = get_approximately_equidistant_circuits(nb_points_on_bloch_sphere)
+for c in circuits:
+    c.delay(32 * 100, 0)
 
 print("Generating tomography circuits...")
-qubit_number: int = 5
 basis = TetrahedralMeasurementBasis()
 tomography_circuits = [
     one_qubit_tomography_circuits(circuit, basis, qubit_number) for circuit in circuits
 ]
 
+# %%
 print("Simulating tomography circuits...")
-result = simulator.run(sum(tomography_circuits, start=[]), shots=2 ** 13).result()
+result = simulator.run(sum(tomography_circuits, start=[]), shots=2**13).result()
 
+# %%
 print("Computing the density matrices...")
 density_matrices_all_qubits: ty.List[ty.List[numpy.ndarray]] = [
     post_process_tomography_results_mle(result, circuit, basis, qubit_number)
     for circuit in circuits
 ]
 
+# %%
 print("Plotting...")
 qubit_index = 0
 points: ty.List[numpy.ndarray] = list()
